dfa8c2fe09d550988d1072fbce5988a2fb3222db
Amani1031 <aarora09@calpoly.edu>
2024-04-09 02:24:43 -0700

    mongoDB dev

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -6,19 +6,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const logReg = require("./login-register");
+const system = require("./systemFuncs");
 
 const {
     api: { host, port },
 } = config;
 
-app.post("/api/login", logReg.login);
+app.post("/api/login", system.login);
 
-app.post("/api/register", logReg.register);
+app.post("/api/register", system.register);
 
-app.get("/api/users", logReg.get_all_users);
+app.get("/api/users", system.get_all_users);
 
-app.get("/api/users/:username", logReg.get_single_user);
+app.get("/api/users/:username", system.get_single_user);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
